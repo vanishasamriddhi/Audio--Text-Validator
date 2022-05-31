faa0b7373df547bcc69f985dfcbe6b012b1c8ff5
Vanisha Samriddhi <samriddhivanisha@gmail.com>
2022-06-01 01:21:46 +0530

    table

---
+++ table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `demo` (
+    `audioI` varchar(20) NOT NULL,
+    `audioII` varchar(20) NOT NULL,
+    `audioIII` varchar(20) NOT NULL,
+    `audioIV` varchar(20) NOT NULL,
+    `audioV` varchar(20) NOT NULL
+);
